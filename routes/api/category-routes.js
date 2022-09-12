f67d44cde5a237da57d186e79ce71805966e2990
Combatxd <logan.schoerner@gmail.com>
2022-09-12 12:20:16 -0600

    finished modals and one route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,77 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const findAllCategories = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(findAllCategories);
+  } catch {
+    res.status(500).json(err.message);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const findOneCategory = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    res.status(200).json(findOneCategory);
+  } catch {
+    res.status(500).json(err.message);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+ 
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch {
+    res.status(500).json(err.message);
+  }
+
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  
+  try {
+    const updatedCategory = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    res.status(200).json(updatedCategory);
+  } catch {
+    res.status(500).json(err.message);
+  }
+
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -89,8 +89,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Book.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 });
 
 module.exports = router;
